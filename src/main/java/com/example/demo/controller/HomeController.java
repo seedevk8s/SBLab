115d826d4cb12a6850d484472e034452fc73e363
seedevk8s <seedevk8s@gmail.com>
2024-10-05 15:42:36 +0900

    컨트롤러 추가

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -1,5 +1,15 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
 
+	//Controller 메서드, Handler 메서드 : 파이썬은 @app.get("/")
+	@GetMapping("/")								// http://localhost:8080/로 요청하면 index.html 문서를 화면에 보여주세요.
+	public String home() {
+		return "index";
+	}	
+	
 }
